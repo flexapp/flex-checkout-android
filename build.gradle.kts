207630d6dff84152a736940ec1039d8fcd81358d
Chris Myers <chris.myers@getflex.com>
2026-05-27 09:07:47 -0600

    Release 1.3.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,7 +7,7 @@ publishing {
         create<MavenPublication>("release") {
             groupId = "com.getflex.checkout"
             artifactId = "flex-checkout-android"
-            version = "1.2.0"
+            version = "1.3.0"
 
             artifact(file("libs/flex-checkout-android-release.aar")) {
                 extension = "aar"
