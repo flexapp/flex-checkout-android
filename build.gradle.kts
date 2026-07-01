f1b6071583a8b36f8d8dc351c5a163110f2b4571
Chris Myers <30842100+chrismyer@users.noreply.github.com>
2026-07-01 16:25:35 -0600

    Release 1.5.2
    Coding-Agent: Claude Code

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,7 +7,7 @@ publishing {
         create<MavenPublication>("release") {
             groupId = "com.getflex.checkout"
             artifactId = "flex-checkout-android"
-            version = "1.5.1"
+            version = "1.5.2"
 
             artifact(file("libs/flex-checkout-android-release.aar")) {
                 extension = "aar"
